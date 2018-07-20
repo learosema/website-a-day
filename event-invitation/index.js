71841b8e4115c224a72fb889402063ee07a86dab
Lea Rosema <terabaud@gmail.com>
2018-07-20 20:41:04 +0200

    change menu

---
+++ event-invitation/index.js
@@ -0,0 +1,20 @@
+const $ = document.querySelector.bind(document)
+const $$ = (sel, con) => (con||document).querySelectorAll(sel)
+
+NodeList.prototype.toArray = function () {
+  return Array.prototype.slice.call(this)
+}
+
+NodeList.prototype.addEventListener = function (eventType, listener, options) {
+  this.toArray().forEach(element => {
+    element.addEventListener(eventType, listener, options)
+  });
+}
+
+const menu = $`#menu`
+
+$$`.nav__link`.addEventListener('click', (e) => {
+  console.log('MUH!!!')
+  menu.checked = false;
+  return true;
+})
